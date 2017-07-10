5f9b8f19e4bb522c59ea7effa4597fb81c79fc19
wxywr <1261046195@qq.com>
2017-07-10 16:37:46 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 $(window).resize(function(){
-    $('html').css({'font-size':$(window).width()/3.9+"px"});
+    $('html').css({'font-size':$(window).width()/3.2+"px"});
 });
 
 $(function(){
-	$('html').css({'font-size':$(window).width()/3.9+"px"});
-		
+	$('html').css({'font-size':$(window).width()/3.2+"px"});
+	$('.yue').click(function(){
+		$('.alert').fadeIn('1000');
+		$('.sort').fadeIn('1000');
+		$('.logo').fadeIn('1000');
+	});
+	$('.sort .no').click(function(){
+		$('.alert').fadeOut('1000');
+		$('.sort').fadeOut('1000');
+		$('.logo').fadeOut('1000');
+	});	
 
 })
