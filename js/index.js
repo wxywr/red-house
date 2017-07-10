939f6b9808541d359471449842c66c02b13f396b
Your Name <you@example.com>
2017-07-10 15:19:01 +0800

    update

--- js/index.js
+++ js/index.js
@@ -4,5 +4,14 @@ $(window).resize(function(){
 
 $(function(){
 	$('html').css({'font-size':$(window).width()/3.2+"px"});
-
-})
+	$('.yue').click(function(){
+		$('.alert').fadeIn('1000');
+		$('.sort').fadeIn('1000');
+		$('.logo').fadeIn('1000');
+	});
+	$('.sort .no').click(function(){
+		$('.alert').fadeOut('1000');
+		$('.sort').fadeOut('1000');
+		$('.logo').fadeOut('1000');
+	});
+});
